0e43918588ffe54851be318cdacf44ab4c842998
YugalRajVimal <sagaryugal472@gmail.com>
2023-12-12 14:20:26 +0530

    ..

--- client.js
+++ client.js
@@ -1,23 +1,36 @@
+// Import necessary modules here.
 import readline from "readline";
 import { io } from "socket.io-client";
 
+// Creating an interface to read input from the user in Node.js using the readline module.
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
+  terminal: false,
 });
 
-const socket = io("https://cli-chat-app.onrender.com");
+// Creating a Socket.IO connection to the server at "https://chat-app-socketio-inpt.onrender.com".
+const socket = io("https://socketio-chat-app-el5v.onrender.com");
 
+// Sets up an event listener for the "connect" event in Socket.io
 socket.on("connect", () => {
   console.log("Connected to Server!!!");
 
-  rl.question("Enter your Username: ", (username) => {
-    process.stdout.write("Me: ");
+  let username;
 
+  // Taking input as username
+  rl.question("Enter your Username: ", (line) => {
+    rl.on("line", () => {
+      username = line;
+      socket.emit("join", username);
+    });
+    //Read next line for new message
+    process.stdout.write("Me : ");
     rl.on("line", (msg) => {
       process.stdout.clearLine();
       process.stdout.cursorTo(0);
 
+      // Send custom events from the client to the server - ("chat-msg")
       socket.emit("chat-msg", `${username}: ${msg}`);
 
       process.stdout.write("Me: ");
@@ -25,10 +38,12 @@ socket.on("connect", () => {
   });
 });
 
+//Recieve Broadcasted Message from the Server on custom event ("chat-msg")
 socket.on("chat-msg", (msg) => {
   process.stdout.clearLine();
   process.stdout.cursorTo(0);
 
+  //Print the message on every clients terminal. (Broadcast Message)
   console.log(msg);
 
   process.stdout.write("Me: ");
--- server.js
+++ server.js
@@ -1,24 +1,37 @@
+// Import necessary modules here.
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 
+// Create an instance of the Express web application framework in Node.js
 const app = express();
+
+// Create HTTP server
 const server = createServer(app);
 
-
-
+// Create a new instance of the Socket.IO server
 const io = new Server(server);
 
+// Sets up an event listener for the "connection" event in Socket.IO
 io.on("connection", (socket) => {
   console.log("Connection Started............!!");
 
-  socket.on("chat-msg", (msg) => {
-    console.log("Message: " + msg);
+  // Recieve custom events from the client - ("chat-msg")
 
-    socket.broadcast.emit("chat-msg", msg);
+  socket.on("join", (username) => {
+    console.log(username + " Joined");
 
-    socket.on("disconnect", () => {
-      console.log("User disconnected.......");
+    socket.on("chat-msg", (msg) => {
+      //Print message on server's console
+      console.log("Message: " + msg);
+
+      //Broadcast the Message to every client on custom event ("chat-msg")
+      socket.broadcast.emit("chat-msg", msg);
+
+      // Sets up an event listener for the "disconnect" event in Socket.IO
+      socket.on("disconnect", () => {
+        console.log("User disconnected.......");
+      });
     });
   });
 });
